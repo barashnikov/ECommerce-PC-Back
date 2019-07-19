481a9d8c6057ea5a22e025ad1a877ef82a086508
Fakiri Mohamed <39508@heb.be>
2019-07-19 16:59:46 +0200

    Insert SQl

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 insert into ITEMS(id,item_name,price,picture,descrpition,type,stock) VALUES ('test','ACER PC',500,'https://techarea.fr/wp-content/uploads/2018/05/Design-1-Acer-Aspire-7.jpg','PC DE FOLIE','PC',500);
 insert into ITEMS(id,item_name,price,picture,descrpition,type,stock) VALUES('testd','ALIENWARE PC',9000,'http://i.dell.com/sites/csimages/Video_Imagery/all/aw-17.jpg','PC GAMER','PC',1500);
+insert into ITEMS(id,item_name,price,picture,descrpition,type,stock) VALUES ('testb','ACER PC',500,'https://techarea.fr/wp-content/uploads/2018/05/Design-1-Acer-Aspire-7.jpg','PC DE FOLIE','PC',500);
+insert into ITEMS(id,item_name,price,picture,descrpition,type,stock) VALUES('testc','ALIENWARE PC',9000,'http://i.dell.com/sites/csimages/Video_Imagery/all/aw-17.jpg','PC GAMER','PC',1500);
+insert into ITEMS(id,item_name,price,picture,descrpition,type,stock) VALUES ('teste','ACER PC',500,'https://techarea.fr/wp-content/uploads/2018/05/Design-1-Acer-Aspire-7.jpg','PC DE FOLIE','PC',500);
+insert into ITEMS(id,item_name,price,picture,descrpition,type,stock) VALUES('testf','ALIENWARE PC',9000,'http://i.dell.com/sites/csimages/Video_Imagery/all/aw-17.jpg','PC GAMER','PC',1500);
